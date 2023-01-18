2f358cd813dc94bb1e1bebc863e428ef31a1bab5
dan-2ee <ekdms7273@naver.com>
2023-01-19 02:09:00 +0900

    2920 solution

---
+++ Baekjoon/Class 1/2920.py
@@ -0,0 +1,10 @@
+import sys
+
+lst = list(map(int, sys.stdin.readline().split()))
+
+if (lst == sorted(lst)):
+    print("ascending")
+elif (lst == sorted(lst, reverse=True)):
+    print("descending")
+else:
+    print("mixed")
