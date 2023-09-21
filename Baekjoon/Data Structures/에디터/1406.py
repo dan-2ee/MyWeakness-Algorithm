c035fd2f6c307fa040876c950cbb4930f44b6424
dan-2ee <ekdms7273@naver.com>
2023-09-21 20:31:53 +0900

    solving time complexity

--- Baekjoon/Data Structures/에디터/1406.py
+++ Baekjoon/Data Structures/에디터/1406.py
@@ -1,23 +1,20 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
-dq = deque(list(input().strip()))
+leftWords = list(input().strip())
+rightWords = []
+
 T = int(input())
-N = len(dq)
-cursor = N   # 맨 오른쪽에 위치 
 
 for _ in range(T):
     command = input().split()
-    if (command[0] == "L" and cursor > 0):
-        cursor -= 1
-    elif (command[0] == "D" and cursor < N):
-        cursor += 1
-    elif (command[0] == "B" and cursor > 0):
-        del dq[cursor-1]
-        cursor -= 1
+    if (command[0] == "L" and leftWords):
+        rightWords.append(leftWords.pop())
+    elif (command[0] == "D" and rightWords):
+        leftWords.append(rightWords.pop())
+    elif (command[0] == "B" and leftWords):
+        leftWords.pop()
     elif (command[0] == "P"):
-        dq.insert(cursor, command[1])
-        cursor += 1
-
-print("".join(dq))
+        leftWords.append(command[1])
+    
+print("".join(leftWords)+ "".join(reversed(rightWords)))
