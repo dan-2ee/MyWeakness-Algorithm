0e1b4cbed5ba6100f56dad161a02a8e26c8abc13
dan-2ee <ekdms7273@naver.com>
2023-10-15 17:51:41 +0900

    11660 solution

---
+++ Baekjoon/Dynamic Programming/구간 합 구하기 5/11660.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+N, T = map(int, input().split())
+nums = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0]*(N+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + nums[i-1][j-1]
+
+for _ in range(T):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]
+    print(result)
