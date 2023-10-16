a2d22600c36532ef043b189125997f1395b07c15
dan-2ee <ekdms7273@naver.com>
2023-10-16 19:24:26 +0900

    Dijkstra 절망

--- Baekjoon/Graphs/최단경로/1753.py
+++ Baekjoon/Graphs/최단경로/1753.py
@@ -4,37 +4,37 @@ INF = sys.maxsize
 input = sys.stdin.readline
 V, E = map(int, input().split())
 start = int(input())
-matrix = [[INF]*(V+1) for _ in range(V+1)]
+matrix = [[]*(V+1) for _ in range(V+1)]
 visited = [0]*(V+1)
 distance = [INF]*(V+1)
 
 for _ in range(E):
     u, v, w = map(int, input().split())
-    matrix[u][v] = w
+    matrix[u].append((v, w))
 
 def SmallestNode():
     idx = 0
     minValue = INF
-    for i, value in enumerate(distance):
-        if value < minValue and not visited[i]:
-            minValue = value
+    for i in range(1, V+1):
+        if distance[i] < minValue and not visited[i]:
+            minValue = distance[i]
             idx = i
     return idx
 
 def Dijkstra(start):
     visited[start] = 1
-    for i, v in enumerate(matrix[start]):
-        distance[i] = v
+    for node in matrix[start]:
+        distance[node[0]] = node[1]
     
     for _ in range(V-1):
         next = SmallestNode()
         visited[next] = 1
-        for idx, value in enumerate(matrix[next]):
-            if distance[idx] > distance[next] + value:
-                distance[idx] = distance[next] + value
+        for node in matrix[next]:
+            if distance[node[0]] > distance[next] + node[1]:
+                distance[node[0]] = distance[next] + node[1]
     distance[start] = 0  # 시작 노드의 가중치는 0 
-    
-    for i in distance[1:]:
-        print(i) if i != INF else print("INF")
+
 
 Dijkstra(start)
+for i in distance[1:]:
+    print(i) if i != INF else print("INF")
