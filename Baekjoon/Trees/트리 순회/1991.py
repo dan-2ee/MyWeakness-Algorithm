2946cadb1b7cf5265067c7c5472055970a6f2657
dan-2ee <ekdms7273@naver.com>
2023-12-14 23:17:46 +0900

    1991 solution

---
+++ Baekjoon/Trees/트리 순회/1991.py
@@ -0,0 +1,36 @@
+import sys
+
+def preorder(root):
+    if (root == "."):
+        return
+    print(root, end="")
+    preorder(tree[root][0])
+    preorder(tree[root][1])
+
+def inorder(root):
+    if (root == "."):
+        return
+    inorder(tree[root][0])
+    print(root, end="")
+    inorder(tree[root][1])
+
+def postorder(root):
+    if (root == "."):
+        return
+    postorder(tree[root][0])
+    postorder(tree[root][1])
+    print(root, end="")
+
+input = sys.stdin.readline
+N = int(input())
+tree = {}
+for i in range(N):
+    node, left, right = input().split()
+    tree[node]=[ left, right]
+    
+preorder("A")
+print()
+inorder("A")
+print()
+postorder("A")
+print()
