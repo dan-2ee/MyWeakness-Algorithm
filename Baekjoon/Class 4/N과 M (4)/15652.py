e03bd88022fc94de53ba43c7f891500635f95bfe
dan-2ee <ekdms7273@naver.com>
2023-12-15 19:53:40 +0900

    remove folder

--- Baekjoon/Class 4/N과 M (4)/15652.py
+++
@@ -1,7 +0,0 @@
-from itertools import combinations_with_replacement
-
-N, M = map(int, input().split())
-nums = [i for i in range(1, N+1)]
-comb = [data for data in combinations_with_replacement(nums, M)]
-for i in sorted(comb):
-    print(" ".join(map(str, i)))
